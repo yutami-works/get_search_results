2e1b246ada546f1fb6013af53653ceb216fda74f
yutami <sabacan623@gmail.com>
2022-07-17 09:22:32 +0900

    価格クラスの変更対応

--- get_yshopping_price.py
+++ get_yshopping_price.py
@@ -24,7 +24,7 @@ driver = webdriver.Chrome(service=service, options=options)
 ## 変数定義 ##
 URL = 'https://shopping.yahoo.co.jp/'
 NAME_SEARCH = 'p'
-CN_PRICE = "_15NzwMnSG6fE"
+CN_PRICE = "_3-CgJZLU91dR"
 exe_time = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
 
 # loading words list
